cc44da74645a363429585211590f9959a09ba8a9
KimJeongDae22 <rlawjdeo0895@naver.com>
2025-06-10 15:32:21 +0900

    feat: 플레이어 상태창 기능 추가

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private GameObject _uiDefault;
     [SerializeField] private GameObject _uiInventory;
-
+    [SerializeField] private GameObject _uiStatus;
+    [SerializeField] private TextMeshProUGUI _uiStatusAtk;
+    [SerializeField] private TextMeshProUGUI _uiStatusArm;
+    [SerializeField] private TextMeshProUGUI _uiStatusCri;
+    [SerializeField] private TextMeshProUGUI _uiStatusMaxHp;
     private void Start()
     {
+        _uiStatus.SetActive(false);
+    }
+    public void StatusUpdate()
+    {
+        CommonStat playerstat = Singleton<Player>.Instance.Stat.PlayerStat;
 
+        _uiStatusAtk.text = $"공격력 : {playerstat.TotalAtk}\t [ + {playerstat.EquipAtk}]";
+        _uiStatusArm.text = $"방어력 : {playerstat.TotalArm}\t [ + {playerstat.EquipArm}]";
+        _uiStatusCri.text = $"치명타 확률 : {playerstat.TotalCri}\t [ + {playerstat.EquipCri}]";
+        _uiStatusMaxHp.text = $"최대 체력 : {playerstat.TotalMaxHp}\t [ + {playerstat.EquipMaxHp}]";
     }
     public void Btn_OnDefaultUI()
     {
         _uiDefault.SetActive(true);
         _uiInventory.SetActive(false);
+        _uiStatus.SetActive(false);
     }
     public void Btn_OnInventoryUI()
     {
         _uiInventory.SetActive(true);
+        _uiDefault.SetActive(false);
+    }
+    public void Btn_OnStatusUI()
+    {
+        StatusUpdate();
+        _uiStatus.SetActive(true);
         _uiDefault.SetActive(false);
     }
 }
